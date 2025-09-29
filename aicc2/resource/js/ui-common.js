ec759757f8da49435be1865c7e00eef20852f167
jyoons <aroo2005@naver.com>
2025-09-29 16:15:10 +0900

    모바일버전 추가

--- aicc2/resource/js/ui-common.js
+++ aicc2/resource/js/ui-common.js
@@ -4,6 +4,7 @@ const NX_UI = function(nx){
     nx.accordion._settings();
     nx.tab.init();
     nx.accordion.init();
+    nx.nav.init();
   }
   nx.countNum = {
     init:function(){
@@ -45,21 +46,17 @@ const NX_UI = function(nx){
       this._setEvent();
     },
     _setEvent:function(){
-      var elem1 = '.navList>li'
-      var elem2 ='.navList .nav-dep1, .navList .nav-dep2'
-     $(document).on('mouseenter mouseleave', elem1, function(e){
+      $(document).off('mouseenter mouseleave click', '.nav-btn, .nav').on('mouseenter mouseleave click', '.nav-btn, .nav', function(e){
         if(e.type === 'mouseenter') {
-          $(this).find('.nav-dep1').addClass('active');
-        }else if(e.type === 'mouseleave'){
-          $(this).find('.nav-dep1').removeClass('active');
-        }
-      });
-     $(document).on("click", elem2, function(){
-        var flag = $(this).parents('li').find('.nav-dep2-list').length;
-        if(flag === 0 || $(this).hasClass('nav-dep2')) {
-          $(elem1).removeClass('active');
-          $(this).closest('.navList').find('li').removeClass('active');
-          $(this).parents('li').addClass('active');
+          console.log('mouseenter')
+          $('.nav').addClass('active');
+        }else if(e.type === 'mouseleave') {
+          var flag =  e.target.className;
+          if(flag.indexOf('nav-btn') < 0 ) {
+            $('.nav').removeClass('active');
+          }
+        }else if(e.type === 'click') {
+          $('.nav').hasClass('active') ? $('.nav').removeClass('active') : $('.nav').addClass('active');
         }
       });
     }
@@ -130,7 +127,6 @@ const NX_UI = function(nx){
       });
     }
   },
-
   nx.init();
   return nx;
 }(window.NX_UI || {}, jQuery);
